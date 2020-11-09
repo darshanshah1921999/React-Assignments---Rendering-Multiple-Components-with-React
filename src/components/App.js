9882eb0c0a53f6513acade14c870d9ba34309e0b
darshanshah1921999 <54939839+darshanshah1921999@users.noreply.github.com>
2020-11-09 10:34:24 +0530

    Render multiiple element in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h2 data-ns-test="project-name">Name of project:</h2>
+      <span data-ns-test="project-description">Description of Project:</span>
+    </>
+  );
+};
 
 export default App;
